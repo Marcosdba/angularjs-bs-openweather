1ed0a0d9df5e16d17a30349be362b5afd7f4b863
Marcos Bidas <marcostadeu.dba@gmail.com>
2016-08-06 21:42:21 -0300

    Controller - hide alert on success
    Controller - fix the alert on internet connection success

--- public/js/controllers/clima-controller.js
+++ public/js/controllers/clima-controller.js
@@ -3,11 +3,12 @@ angular.module('clima').controller('ClimaController', function($scope, $http){
   $scope.submeter = function(){  
       $http.get('http://api.openweathermap.org/data/2.5/weather/?q='+$scope.clima.cidade+'&APPID=3cf30ad4035fe6da2de2858236727cad&units=metric')
     .then(function(response) {
-      $scope.clima = response.data;      
+      $scope.clima = response.data;
       $scope.showThePanel = true;
+      $scope.showThePanelAlert = false;
     }).
     catch(function(response) {
-      $scope.clima = response.data;     
+      $scope.clima = response.data;
       $scope.showThePanel = false;
       $scope.showThePanelAlert = true;
     });
